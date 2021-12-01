a5fb7b07813476da1e2e9fa20d52abffc69cc07f
l1dge <lcullip@gmail.com>
2021-12-01 08:42:10 +0000

    Initial Commit

---
+++ meetingbuddy.py
@@ -0,0 +1,27 @@
+from rich import inspect, Console, pretty, Progress
+
+#Argparser
+
+
+
+#Meeting Attendees
+
+
+
+#Speaker Timer
+
+
+
+#Next Speaker
+
+
+
+#Countdown Display
+
+
+
+#Random Attendee Selector
+
+
+
+#Screen Display
