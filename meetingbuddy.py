3dc170eda61c54835ce4bddc4fc7192d340ba0b1
l1dge <lcullip@gmail.com>
2021-12-06 13:13:52 +0000

    Tweaked the ordering

--- meetingbuddy.py
+++ meetingbuddy.py
@@ -15,7 +15,7 @@ data = input("> ")
 
 attendees = data.split()
 
-print("[green]Enter length of meeting in minutes[/green]")
+print("[green]Enter length of meeting in minutes (must be 10 or greater)[/green]")
 print("[green]Example for meeting of 2hours:[/green]")
 print("[red]120[/red]")
 
@@ -25,13 +25,21 @@ slot = round(timing / (len(attendees)+1))
 
 shuffle(attendees)
 for upnext in attendees:
+    print("[blue]Are we ready for the next speaker?[/blue]")
+    prompt.Prompt.ask("Press Enter to continue......")
+
     print(f"[blue]Up Next:[/blue] [red] {upnext}[/red] {slot}")
     for step in progress.track(range((slot*60))):
         sleep(1)
         step
 
-        print("[blue]Are we ready for the next speaker?[/blue]")
-        prompt.Prompt.ask("Press Enter to continue......")
+print("[blue]Are we ready for the next speaker?[/blue]")
+prompt.Prompt.ask("Press Enter to continue......")
+
+print(f"[blue]Back to the Chair[/blue] {slot}")
+for step in progress.track(range((slot*60))):
+    sleep(1)
+    step
 
 #Next Speaker
 def NextSpeaker():
